157b50c4711b316ae35e72a5129922439518ab08
DoubleYorsun <62883913+DoubleYorsun@users.noreply.github.com>
2022-07-21 18:39:20 +0800

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,20 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api');
-
 const token = '5532038023:AAHLKLGMyp0DRmFdi999HseI0fK2szIyvc0';
-const bot = new TelegramBot(token, {
-  polling: true
-});
 
-
-
-bot.onText(/\/hentai/, function onLoveText(msg) {
-  bot.sendMessage(msg.chat.id, 'Are you a hetai?');
-});
-
-
-bot.onText(/\/echo (.+)/, (msg, match) => {
-
-  const chatId = msg.chat.id;
-  const resp = match[1];
-  bot.sendMessage(chatId, resp);
-});
+const bot = new TelegramBot(token, {polling: true});
+const randomBoolean = () => Math.random() >= 0.5;
+const message_id = 237771
+const chatId = -1001464302418
+bot.deleteMessage(chatId,message_id)
+//if (randomBoolean = ture,is_pre=true) {
+//bot.deleteMessage(chatId,message_id)    
+//}
